c7e8540dabf47122d273f479f3516eb3ea805be8
syntaxaire <syntaxaire@gmail.com>
2020-02-04 13:39:12 -0500

    Remove unused module

--- qbe/wiki_helpers.py
+++
@@ -1,6 +0,0 @@
-from wiki_config import site
-
-
-def get_all_pages():
-    """Helper function to return a list of all pages on the wiki."""
-    return [page.page_title for page in site.allpages()]
