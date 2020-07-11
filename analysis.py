3ae5504ed78ee84cb157806fa7fbd2ed3c7b027f
syntaxaire <syntaxaire@gmail.com>
2020-07-11 13:22:13 -0400

    Add new analysis scripts

--- analysis.py
+++ analysis.py
@@ -1,15 +1,14 @@
-"""Various analyses made possible by the Qud object tree and tile rendering system."""
+"""Various analyses made possible by the Qud object tree and tile rendering system.
+
+This script file is not part of the main project. Some of these may be out of date."""
+
 import string
 
 import anytree
+from hagadias import qudtile, gameroot
 import mwclient
 
-from hagadias import qudtile, gameroot
-import wikipage
-from hagadias.gameroot import qindex
-
-# This script is not part of the main application, so set your XML location here
-FILE = 'C:/Steam/steamapps/common/Caves of Qud/CoQ_Data/StreamingAssets/Base/ObjectBlueprints.xml'
+from qbe.qudobject_wiki import QudObjectWiki
 
 
 def get_bad_tiles():
@@ -74,7 +73,56 @@ def print_empty_descriptions():
                 print(name, qud_object.desc)
 
 
-gameroot.load(FILE)
+def print_new_and_deleted(qindex, old_qindex):
+    """Print numbers of items added and deleted between versions of the game,
+    as well as the names of the items."""
+    new_objects = [name for name in qindex if name not in old_qindex]
+    print("New objects:")
+    print(len(new_objects), new_objects)
 
-# Run any analyses here:
-breakpoint()
+    deleted_objects = [name for name in old_qindex if name not in qindex]
+    print("Deleted objects:")
+    print(len(deleted_objects), deleted_objects)
+
+
+def print_new_tree(root, qindex, old_qindex):
+    """Print a tree of only items added in a new version of the game."""
+    new_objects = set([name for name in qindex if name not in old_qindex])
+    parents_of_new_objects = set()
+    for object in new_objects:
+        node = qindex[object].parent
+        while node is not None:
+            if node.name not in new_objects:
+                parents_of_new_objects.add(node.name)
+            node = node.parent
+    for pre, fill, node in anytree.RenderTree(root):
+        if node.name in parents_of_new_objects:
+            pre_html = '<i>'
+            post_html = '</i>'
+        if node.name in new_objects:
+            pre_html = "'''"
+            post_html = "'''"
+        if node.name in new_objects or node.name in parents_of_new_objects:
+            include = '✅' if node.is_wiki_eligible() else '❌'
+            print(f' {pre}{pre_html}{node.name}{post_html} {include}')
+
+
+# def check_description_coverage(root, qindex):
+#     """
+#     Check for unique descriptions that do not have wiki coverage.
+#     Check for wiki eligible objects with identical descriptions.
+#     """
+
+
+
+new_gameroot = gameroot.GameRoot(r'C:\Steam\steamapps\common\Caves of Qud')
+old_gameroot = gameroot.GameRoot(r'D:\Games\Caves of Qud stable')
+
+gamever = new_gameroot.gamever
+stable_gamever = old_gameroot.gamever
+print(f'Comparing {gamever} to {stable_gamever}...')
+
+root, qindex = new_gameroot.get_object_tree(QudObjectWiki)
+old_root, old_qindex = old_gameroot.get_object_tree()
+
+print_new_tree(root, qindex, old_qindex)
