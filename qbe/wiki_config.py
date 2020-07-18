dfa80e910816a240c51da7e832f5e0fac2136a75
syntaxaire <syntaxaire@gmail.com>
2020-07-17 23:13:17 -0400

    Remove trailing newline from QBE block

--- qbe/wiki_config.py
+++ qbe/wiki_config.py
@@ -7,4 +7,3 @@ with open('wiki.yml') as f:
 
 site = Site(wiki_config['base'], path=wiki_config['path'])
 site.login(wiki_config['username'], wiki_config['password'])
-bee = site.pages['portable beehive']
