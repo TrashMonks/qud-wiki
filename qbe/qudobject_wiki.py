957bd9864d66bc4e6ba8f7b9afd5f57ec9e26ef7
egocarib <egocaribx@gmail.com>
2022-06-07 14:02:03 -0500

    exception for Decrypted Signal Data Disk

--- qbe/qudobject_wiki.py
+++ qbe/qudobject_wiki.py
@@ -111,6 +111,8 @@ class QudObjectWiki(QudObjectProps):
         """Return whether this object should be included in the wiki."""
         if self.name == 'Argyve\'s Data Disk Encoded':
             return True  # special case because of '['
+        if self.name == 'Decrypted Signal Data Disk':
+            return True  # special case because of '['
         if self.name == 'DefaultFist':
             return True  # special case because this is the player's fist
         if self.tag_BaseObject:
